6538fea90cd86fe51b0d4738e9b1a973bb8fd4f8
serhii_loboda <serhiy.loboda@gmail.com>
2024-04-04 18:32:04 +0300

    implemented service fastapi

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -8,6 +10,47 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+def read_file_json() -> dict:
+    with open("McDonald_menu.json", "r", encoding="UTF-8") as json_file:
+        content = json_file.read()
+        return json.loads(content)
+
+
+@app.get("/all_products/")
+def get_all_products():
+    return read_file_json()
+
+
+@app.get("/products/{product_name}/")
+def get_product_by_name(
+        product_name: str
+) -> dict | HTTPException:
+    all_products = read_file_json()
+    product = all_products.get(product_name)
+
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found!")
+
+    return product
+
+
+@app.get("/products/{product_name}/{product_field}/")
+def get_product_field_by_name(
+        product_name: str, product_field: str
+) -> dict | HTTPException:
+    all_products = read_file_json()
+    product = all_products.get(product_name)
+
+    if product is None:
+        raise HTTPException(
+            status_code=404, detail="Product not found!"
+        )
+
+    product_field_data = product.get(product_field)
+
+    if product_field_data is None:
+        raise HTTPException(
+            status_code=404, detail="Product field not found!"
+        )
+
+    return {product_field: product_field_data}
